b13b9ab0699729ee4f34f0dd4255657421d38c1e
nicole <kikigaming94@outlook.com>
2024-07-29 22:51:19 -0400

    fixing script for task 0

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Creates a new directory named '0-new_project'
-mkdir 0-new_project
+# mkdir 0-new_project
 
 # Changes to the newly created directory
-cd 0-new_project
+# cd 0-new_project
 
 # Initializes a new C# project
 dotnet new console --output 0-new_project
