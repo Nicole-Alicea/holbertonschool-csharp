f603b0f98b31ef11652a86ddfda396e8da005dd9
nicole <kikigaming94@outlook.com>
2024-07-30 22:15:26 -0400

    adding new line for task 6

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -8,6 +8,7 @@ class Program
         string repeated_str = string.Concat(Enumerable.Repeat(str, 3));
         string substring = str.Substring(0, 9);
 		Console.WriteLine(repeated_str);
+        Console.WriteLine();
         Console.WriteLine(substring);
         }
 }
