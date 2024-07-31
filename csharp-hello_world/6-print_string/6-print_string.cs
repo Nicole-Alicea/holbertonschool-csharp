ce8a7047ca77ed1f6f10c33fd7b4ac3f761879bc
nicole <kikigaming94@outlook.com>
2024-07-30 22:26:42 -0400

    fixing code

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,9 +5,6 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-        string repeated_str = String.Concat(Enumerable.Repeat(str, 3));
-        string substring = str.Substring(0, 9);
-		Console.WriteLine(repeated_str);
-        Console.WriteLine(substring);
+        Console.WriteLine(str+str+str+"\n"+str.Substring(0, 9));
         }
 }
