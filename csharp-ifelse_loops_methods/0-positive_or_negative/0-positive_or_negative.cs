7b24171805e0fb12cadcc79fbf6e99fa94e82c7d
nicole <kikigaming94@outlook.com>
2024-07-31 10:48:37 -0400

    forgot {} in the if else statements

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -8,10 +8,16 @@ class Program
         int number = rndm.Next(-10, 10);
     
         if (number > 0)
+        {
             Console.WriteLine($"{number} is positive");
+        }
         else if (number == 0)
+        {
             Console.WriteLine($"{number} is zero");
+        }
         else
+        {
             Console.WriteLine($"{number} is negative");
+        }
     }
 }
