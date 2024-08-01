825ed1c5a6a66bd18889f095dbda97bf7e239b17
nicole <kikigaming94@outlook.com>
2024-07-31 23:18:50 -0400

    task 4

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int number = 0; number <= 98; number++)
+        {
+            Console.Write(number + " = " + number.ToString("X"));
+        }
+    }
+}
