007976ddf43892a6e0b3b653013cf4e6bb0e4a05
nicole <kikigaming94@outlook.com>
2024-08-01 20:29:52 -0400

    task 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int number = 0; number <= 99; number++)
+        {
+            if (number == 99)
+            {
+                Console.Write($"{number:D2}\n");
+            }
+            Console.Write($"{number:D2}, ");
+        }
+    }
+
+}
