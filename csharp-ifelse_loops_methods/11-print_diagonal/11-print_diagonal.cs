0d0410b3d8d12ca373c7a87da0ba848d8483930d
nicole <kikigaming94@outlook.com>
2024-08-01 23:57:40 -0400

    task 11

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            for (int x = 0; x < length; x++)
+            {
+                Console.Write("\\");
+            }
+            Console.WriteLine();
+        }
+    }
+}
