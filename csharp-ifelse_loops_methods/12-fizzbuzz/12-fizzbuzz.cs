5c7243f4769b4c268398bc22603370c5c742c346
nicole <kikigaming94@outlook.com>
2024-08-02 00:08:43 -0400

    task 12

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main();
+    {
+        for (int x = 0; x < 100; x++)
+        {
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                Console.Write("FizzBuzz ");
+            }
+            else if (x % 3 == 0)
+            {
+                Console.Write("Fizz ");
+            }
+            else if (x % 5 == 0)
+            {
+                Console.Write("Buzz ");
+            }
+            else
+            {
+                Console.Write($"{x} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
