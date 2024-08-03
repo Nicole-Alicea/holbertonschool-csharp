e96f42e42bea07319c2a42ae7f7ae1bb57de1142
nicole <kikigaming94@outlook.com>
2024-08-03 00:40:28 -0400

    adding README and task 0

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size);
+    {
+        if (size == 0)
+        {
+            Console.WriteLine();
+        }
+        else if (sizeof < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null
+        }
+        else
+        {
+            for (int x = 0; x < size; x++);
+            {
+                Console.Write($"{x} ");
+            }
+        }
+    }
+}
