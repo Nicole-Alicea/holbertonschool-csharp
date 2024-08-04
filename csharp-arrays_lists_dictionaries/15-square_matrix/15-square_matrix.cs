70f5a7cdd50a9aed749984b92890923407a711ea
nicole <kikigaming94@outlook.com>
2024-08-03 23:01:36 -0400

    task 15

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int row = myMatrix.GetLength(0);
+		int col = myMatrix.GetLength(1);
+        int[,] nueva = new int[row, col];
+        
+        for (int x = 0; x < row; x++)
+        {
+            for (int y = 0; y < col; y++)
+                    nueva[x, y] = Convert.ToInt16(Math.Pow(myMatrix[x, y], 2));
+        }
+        return nueva;
+
+    }
+}
