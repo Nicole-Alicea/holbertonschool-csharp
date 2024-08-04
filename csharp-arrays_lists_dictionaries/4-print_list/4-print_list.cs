6b753d25de3e84d54e09bc67affcbb662e92f499
nicole <kikigaming94@outlook.com>
2024-08-03 20:56:14 -0400

    task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> list = new List<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            list.Add(i);
+        }
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.Write(list[i]);
+            if (i < list.Count - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+
+        return list;
+    }
+}
