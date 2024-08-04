6cd11a1e0372316da2d6c2ae4cdb6b9d2a54f8ee
nicole <kikigaming94@outlook.com>
2024-08-03 20:32:04 -0400

    task 3

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        for (int x = array.Length - 1; x >= 0; x--)
+        {
+            Console.Write(array[x]);
+            if (x > 0)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
