10bd8c1409ecdda9f22d8ebe36849937f4b01ff5
nicole <kikigaming94@outlook.com>
2024-08-03 22:14:57 -0400

    task 9

---
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        int x = 0;
+
+        foreach (KeyValuePair<string, string> entry in myDict)
+        {
+            if(entry.Key == key)
+                x = 1;
+        }
+        if(x == 1)
+            myDict[key] = value;
+        else 
+            myDict.Add(key, value);
+        
+        return myDict;
+        
+    }
+}
