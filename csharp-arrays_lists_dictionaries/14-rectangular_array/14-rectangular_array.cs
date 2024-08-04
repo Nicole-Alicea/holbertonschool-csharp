284faba70bf70d5624d43d588289e9dd4e815ea8
nicole <kikigaming94@outlook.com>
2024-08-03 22:48:29 -0400

    task 14

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] mat = new int[5, 5];
+                    
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+                    mat[x, y] = 0;
+        }
+        mat[2,2] = 1;
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+                    Console.Write("{0}{1}", mat[x,y], y < 4 ? " ": "");
+            Console.WriteLine();
+        }
+           
+    }
+}
