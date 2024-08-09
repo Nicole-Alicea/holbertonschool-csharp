96ec4f4cc99f5823aec6f171b1ba11c8edb6b8bd
nicole <kikigaming94@outlook.com>
2024-08-09 09:57:40 -0400

    task 2

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        // The HashSets will store the unique integers in each list
+        HashSet<int> differentIntegers1 = new HashSet<int>(list1);
+        HashSet<int> differentIntegers2 = new HashSet<int>(list2);
+
+        // Creates a list that will store the different integers
+        List<int> diffIntegers = new List<int>();
+
+        // This will add the integers that are in differentIntegers1 but not in differentIntegers2
+        foreach (int integer in differentIntegers1)
+        {
+            if (!differentIntegers2.Contains(integer))
+            {
+                diffIntegers.Add(integer);
+            }
+        }
+
+        // This will add the integers that are in differentIntegers2 but not in differentIntegers1
+        foreach (int integer in differentIntegers2)
+        {
+            if (!differentIntegers1.Contains(integer))
+            {
+                diffIntegers.Add(integer);
+            }
+        }
+        // Will sort the different integers
+        int sortedIntegers = diffIntegers.Sort();
+        
+        return sortedIntegers;
+    }
+}
