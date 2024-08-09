d430e29e9094d724ee842f99bb8e0cec603aba11
nicole <kikigaming94@outlook.com>
2024-08-08 23:50:35 -0400

    task 1

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        // Creates a HashSet that will store the common elements in both lists
+        HashSet<int> commonIntegers = new HashSet<int>();
+
+        // Iterates through the first list
+        foreach (int integer in list1)
+        {
+            // If the integer is also in the second list, it will be added to the HashSet
+            if (list2.Contains(integer))
+            {
+                commonIntegers.Add(integer);
+            }
+        }
+        // Converts HashSet to a List
+        List<int> sortedList = new List<int>(commonIntegers);
+
+        // Sorts the list
+        sortedList.Sort();
+
+        return sortedList;
+    }
+}
