41855feac494afb165a198339c1c1794ad8d0e6e
nicole <kikigaming94@outlook.com>
2024-08-08 20:56:29 -0400

    adding Readme and task 0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        // HashSets contain only unique elements
+        HashSet<int> uniqueIntegers = new HashSet<int>(myList);
+
+        // Will add all of the unique integers
+        return uniqueIntegers.Sum();
+    }
+}
