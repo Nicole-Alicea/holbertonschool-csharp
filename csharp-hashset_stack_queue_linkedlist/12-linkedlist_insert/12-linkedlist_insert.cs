c0c02bedff8fca79033fbab812c7193e337c7803
nicole <kikigaming94@outlook.com>
2024-08-09 23:03:48 -0400

    task 12

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+
+        while (current != null && current.Value < n)
+        {
+            current = current.Next;
+        }
+
+        if (current == null)
+        {
+            return myLList.AddLast(n);
+        }
+        else
+        {
+            return myLList.AddBefore(current, n);
+        }
+    }
+}
