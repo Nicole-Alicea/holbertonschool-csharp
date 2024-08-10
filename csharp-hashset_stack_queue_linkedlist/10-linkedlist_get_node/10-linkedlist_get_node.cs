33eb6782a8ec1ad2ae4506b73d843050627c2399
nicole <kikigaming94@outlook.com>
2024-08-09 22:54:22 -0400

    task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n < 0 || n >= myLList.Count)
+        {
+            return 0;
+        }
+        int index = 0;
+        foreach (int value in myLList)
+        {
+            if (index == n)
+            {
+                return value;
+            }
+            index++;
+        }
+        return 0;
+    }
+}
