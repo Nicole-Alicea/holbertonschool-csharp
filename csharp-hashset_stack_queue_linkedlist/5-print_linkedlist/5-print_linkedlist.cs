07c4d8c91be20a071197cb72380e59947ad351c6
nicole <kikigaming94@outlook.com>
2024-08-09 22:26:33 -0400

    task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> llist = new LinkedList<int>();
+
+        if (size < 0)
+        {
+            return llist;
+        }
+
+        for (int x = 0; x < size; x++)
+        {
+            llist.AddLast(x);
+            Console.WriteLine(x);
+        }
+
+        return llist;
+    }
+}
