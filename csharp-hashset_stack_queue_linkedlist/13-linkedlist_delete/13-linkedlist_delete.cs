615c2720bb69245504e69103a631452d550544ed
nicole <kikigaming94@outlook.com>
2024-08-09 23:17:13 -0400

    task 13

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index < 0 || index >= myLList.Count)
+        {
+            return;
+        }
+
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; i < index; i++)
+        {
+            current = current.Next;
+        }
+
+        if (current != null)
+        {
+            myLList.Remove(current);
+        }
+    }
+}
