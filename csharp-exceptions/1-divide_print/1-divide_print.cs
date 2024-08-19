fc2ce3436f20e911561871c048e9a5ba44044559
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-08-19 18:12:29 -0400

    task 1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            result = a / b;
+        }
+        catch
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {result}");
+        }
+    }    
+}
