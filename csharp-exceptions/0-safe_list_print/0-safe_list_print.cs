d70efd215604dffa0157f4760353af4fcc2a832c
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-08-19 17:18:44 -0400

    removing count variable

--- csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -5,20 +5,17 @@ class List
 {
     public static int SafePrint(List<int> myList, int n)
     {
-        int count = 0;
-
         for (int x = 0; x < n; x++)
         {
             try
             {
                 Console.WriteLine(myList[x]);
-                count++;
             }
             catch
             {
-                return count;
+                return x;
             }
         }
-        return count;
+        return x;
     }
 }
