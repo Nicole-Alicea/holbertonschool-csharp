e666af440886babf489a09c203f6b252b1237f95
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-08-19 16:49:14 -0400

    adding readme, task folders and trying task 0

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+
+        try
+        {
+            for (int x = 0; x < n; x++)
+            {
+                Console.WriteLine(myList[x]);
+                count++;
+            }
+        }
+        catch(ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("The amount of elements requested to be printed from the list is out of range.");
+        }
+        return count;
+    }
+}
