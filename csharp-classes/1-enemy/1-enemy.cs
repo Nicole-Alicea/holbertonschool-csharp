7e4764fe053eb6245e1cab0b27c6391c5ecaa1a0
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-08-26 15:47:43 -0400

    adding more documentation

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -3,9 +3,13 @@
     /// <summary> Defines a zombie. </summary>
     public class Zombie
     {
+        /// <summary> The zombie's health. </summary>
         public int health;
 
-        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class. </summary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// Sets the health of the zombie to 0.
+        /// </summary>
         public Zombie()
         {
             health = 0;
