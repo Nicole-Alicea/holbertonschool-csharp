c1260e81c04d85a90a506d8275528f022da5dc64
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-08-26 15:23:16 -0400

    task 1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,14 @@
+﻿namespace Enemies
+{
+    /// <summary> Defines a zombie. </summary>
+    public class Zombie
+    {
+        public int health;
+
+        /// <summary>Initializes a new instance of the class Zombie.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
