953c47ec25f03b92442d14a08f4599328d4ea822
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-08-26 16:31:30 -0400

    task 3

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Defines a zombie. </summary>
+    public class Zombie
+    {
+        private int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// Sets the health of the zombie to 0.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary> Initializes a new instance of the <see cref="Zombie"/> class with a specified health value. </summary>
+        /// <param name="value"> The initial health of the zombie. Must be greater than or equal to 0. </param>
+        /// <exception cref="ArgumentException"> Thrown when the value is less than 0. </exception>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+    }
+
+    /// <summary> Retrieves the health value of a Zombie object and returns it. </summary>
+    /// <returns> Returns the value of health of the Zombie object. </returns>
+    public int GetHealth()
+    {
+        return Zombie.health;
+    }
+}
