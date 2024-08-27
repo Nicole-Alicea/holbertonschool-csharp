3ff41036f5f292902e4f2df8df790a8a0a2509e4
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-08-26 21:08:23 -0400

    adding tests

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,15 +1,50 @@
-namespace MyMath.Tests;
+using NUnit.Framework;
 
-public class Tests
+namespace MyMath.Tests
 {
-    [SetUp]
-    public void Setup()
+   [TestFixture]
+    /// <summary> Tests for Add() </summary>
+    public class OperationsTests
     {
-    }
+        [Test]
+        public void positiveInt()
+        {
+            int result = Operations.Add(2, 4);
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void negativeInt()
+        {
+            int result = Operations.Add(2, -10);
+
+            Assert.AreEqual(-8, result);
+        }
+        
+        [Test]
+        public void bothNegativeInt()
+        {
+            int result = Operations.Add(-10, -10);
+
+            Assert.AreEqual(-20, result);
+        }
+
+        [Test]
+        public void CeroInt()
+        {
+            int result = Operations.Add(0, -20);
+
+            Assert.AreEqual(-20, result);
+        }
+
+         [Test]
+        
+        public void BothCeroInt()
+        {
+            int result = Operations.Add(0, 0);
+
+            Assert.AreEqual(0, result);
+        }
+    } 
 }
--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 
-namespace MyMath;
-
-/// <summary> Provides mathematical operations. </summary>
-public class Operations
+namespace MyMath
 {
-    /// <summary> Adds two integers and returns the result. </summary>
-    /// <param name="a"> The first integer to be added. </param>
-    /// <param name="b"> The second integer to be added. </param>
-    /// <returns> The sum of the two integers. </returns>
-    public static int Add(int a, int b)
+    /// <summary> Provides mathematical operations. </summary>
+    public class Operations
     {
-        int result = (a + b);
-        return result;
+        /// <summary> Adds two integers and returns the result. </summary>
+        /// <param name="a"> The first integer to be added. </param>
+        /// <param name="b"> The second integer to be added. </param>
+        /// <returns> The sum of the two integers. </returns>
+        public static int Add(int a, int b)
+        {
+            int result = (a + b);
+            return result;
+        }
     }
 }
