d30dca3e33f2b8c79a25abfdfb01147c5f4cb5bd
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-08-28 14:25:03 -0400

    fixing code

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 
-namespace MyMath;
-
-/// <summary>
-/// Provides mathematical operations.
-/// </summary>
-public class Operations
+namespace MyMath
 {
     /// <summary>
-    /// Adds two integers and returns the result.
+    /// Provides mathematical operations.
     /// </summary>
-    /// <param name="a">The first integer to be added.</param>
-    /// <param name="b">The second integer to be added.</param>
-    /// <returns>The sum of the integers.</returns>
-    public static int Add(int a, int b)
+    public class Operations
     {
-        return a + b;
+        /// <summary>
+        /// Adds two integers and returns the result.
+        /// </summary>
+        /// <param name="a">The first integer to be added.</param>
+        /// <param name="b">The second integer to be added.</param>
+        /// <returns>The sum of the integers.</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
