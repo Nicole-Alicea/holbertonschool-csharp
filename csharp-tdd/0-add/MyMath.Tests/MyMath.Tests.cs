dc3ccadbf3ebd131a5d8896cc67021072d76f510
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-08-28 14:12:09 -0400

    adding more documentation

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -8,24 +8,36 @@ namespace MyMath.Tests
     /// </summary>
     public class OperationsTests
     {
+        /// <summary>
+        /// Test for adding two positive integers.
+        /// </summary>
         [Test]
         public void AddPositiveIntegers()
         {
             Assert.AreEqual(5, Operations.Add(2, 3));
         }
 
+        /// <summary>
+        /// Test for adding a positive integer and a negative integer.
+        /// </summary>
         [Test]
         public void AddNegativeAndPositive()
         {
             Assert.AreEqual(-1, Operations.Add(-2, 1));
         }
 
+        /// <summary>
+        /// Test for when both integers are zero.
+        /// </summary>
         [Test]
         public void BothIntegersZero()
         {
             Assert.AreEqual(0, Operations.Add(0, 0));
         }
 
+        /// <summary>
+        /// Test for adding two negative integers.
+        /// </summary>
         [Test]
         public void AddNegativeIntegers()
         {
