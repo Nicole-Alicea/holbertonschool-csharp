a0cbbb7513153487876144913ca5d94f3812b551
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-07 22:57:43 -0400

    fixing documentation

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -6,10 +6,10 @@
 public class Obj
 {
     /// <summary>
-    /// Checks if the provided object is of type <see cref="int"/>.
+    /// Checks if the provided object is of type int.
     /// </summary>
     /// <param name="obj">The object to check.</param>
-    /// <returns><c>true</c> if the object is of type <see cref="int"/>; otherwise, <c>false</c>.</returns>
+    /// <returns>True if the object is of type int; otherwise, false.</returns>
     public static bool IsOfTypeInt(object obj)
     {
         return obj is int;
