725c422576a4337a74965e4c0b2e64602968e6ab
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-08 10:37:23 -0400

    task 0

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// A class for type-checking methods.
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Checks if the object is of type int.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>True if the object is an int; otherwise, false.</returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
