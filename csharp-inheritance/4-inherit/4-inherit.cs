dd724c426ac802a451a3eea2ad47e40b3c81d654
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-08 12:41:26 -0400

    task 4

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// Empty base class.
+/// </summary>
+public class Animal
+{
+    // Empty class
+}
+
+/// <summary>
+/// Empty class that inherits from empty class Animal.
+/// </summary>
+public class Dog : Animal
+{
+    // Empty class
+}
