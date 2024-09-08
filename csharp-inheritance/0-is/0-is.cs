117de7cb580c86992cc64df06521c153e64ff817
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-07 22:52:22 -0400

    task 0 and README

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Obj
+{
+    /// <summary>
+    /// Checks if the provided object is of type <see cref="int"/>.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns><c>true</c> if the object is of type <see cref="int"/>; otherwise, <c>false</c>.</returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
