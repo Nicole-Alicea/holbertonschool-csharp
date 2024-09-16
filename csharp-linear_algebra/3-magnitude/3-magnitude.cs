7d2625884713dddd90c538325756238f38328762
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-16 19:13:20 -0400

    task 3

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class VectorMath
+{
+    /// <summary>
+    /// Calculates and returns the length of a given vector.
+    /// </summary>
+    /// <param name="vector"> The vector to check. </param>
+    /// <returns> The length of a given 2D or 3D vector rounded to the nearest hundreth, or -1 if no vector is given. </returns>
+    public static double Magnitude(double[] vector)
+    {
+        // Checks if the given vector is 2D or 3D
+        if (vector.Length == 2 || vector.Length == 3)
+        {
+            double sumOfSquaredNumbers = 0;
+
+            foreach (double number in vector)
+            {
+                sumOfSquaredNumbers += Math.Pow(number, 2);
+            }
+
+            // Calculates the magnitude (length) of the vector and rounds it to the nearest hundreth
+            return Math.Round(Math.Sqrt(sumOfSquaredNumbers));
+        }
+
+        // If the vector is not 2D or 3D, this method returns -1
+        return -1;
+    }
+}
