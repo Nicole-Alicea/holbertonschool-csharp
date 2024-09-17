3bbb905c4a11206f9e7c55190500fdf9293c8693
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-16 20:17:33 -0400

    task 6 - fixing last return

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -32,11 +32,7 @@ public class VectorMath
 
             return result;
         }
-
-        // If any of the vectors is not 2D or 3D, or if they are not the same size, return a vector containing -1.
-        else
-        {
-            return new double[] { -1 };
-        }
+        
+        return new double[] { -1 };
     }
 }
