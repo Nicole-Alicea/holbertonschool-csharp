0a80a924dd7d9c536927359729efbf951a1c9cbd
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-21 19:01:27 -0400

    task 23

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// Class for MatrixMath
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Method to transpose a matrix and return the resulting matrix.
+    /// </summary>
+    /// <param name="matrix"> Matrix to be used in the method. </param>
+    /// <returns>
+    /// The resulting matrix.
+    /// If the matrix is empty, it will return an empty matrix.
+    /// </returns>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        double[,] result = new double[cols, rows];
+
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < cols; y++)
+            {
+                result[y, x] = matrix[x, y];
+            } 
+        }
+        return result;
+    }
+}
