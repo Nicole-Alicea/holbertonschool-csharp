aec0b5a84fcb0f1505b9b3b86140a8f45c831372
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-21 08:18:37 -0400

    task 20

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// Class for MatrixMath
+/// </summary>
+class MatrixMath
+{
+	/// <summary>
+    /// Method that rotates a square 2D matrix by a given angle in radius
+    /// </summary>
+    /// <param name="matrix"> The matrix that will be rotated. </param>
+    /// <param name="angle"> The angle in radians used to rotate the matrix. </param>
+    /// <returns>
+    /// The resulting matrix.
+    /// If the matrix is of an invalid size, it returns a matrix containing -1.
+    /// </returns>
+	public static double[,] Rotate2D(double[,] matrix, double angle)
+	{
+		if (matrix.GetLength(1) > 2)
+        {
+			return new double[,] { { -1 } };
+        }
+
+		double[,] matrixRotation = {{Math.Cos(angle), Math.Sin(angle)}, {-1 * Math.Sin(angle), Math.Cos(angle)}};
+		double[,] transformedPoint = new double[2, 2];
+
+		for (int i = 0; i < 2; i++)
+        {
+			for (int j = 0; j < 2; j++)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    transformedPoint[i, j] = Math.Round(transformedPoint[i, j] + (matrix[i, k] * matrixRotation[k, j]), 2);
+                }	
+            }	
+        }
+		return transformedPoint;
+	}
+}
