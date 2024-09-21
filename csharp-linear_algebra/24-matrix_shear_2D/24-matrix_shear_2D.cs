5c2d18e6742df0978a5e15f0905938bd440f54a0
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-21 18:54:45 -0400

    task 22

---
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+/// <summary>
+/// Class for MatrixMath
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Method that shears a square 2D matrix by a given shear factor.
+    /// </summary>
+    /// <param name="matrix"> Square 2D matrix that will be sheared. </param>
+    /// <param name="direction"> The direction of the shear factor. Can be X or Y but not both. </param>
+    /// <param name="factor"> The shear factor. </param>
+    /// <returns>
+    /// The resulting matrix.
+    /// If the matrix is of an invalid size, it will return a matrix containing -1.
+    /// If given an axis that is not x or y, it will return a matrix containing -1.
+    /// </returns>
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        double[,] newMatrix = new double[2,2];
+        double[,] shearMatrix;
+        
+        if (matrix.GetLength(0) != 2 && matrix.GetLength(1) != 2)
+        {
+            return new double[,] { { -1 } };
+        }
+            
+        if (direction == 'x' || direction == 'y')
+        {
+            if (direction == 'x')
+            {
+                shearMatrix = new double[,] {{1, 0}, {factor, 1}};
+            }
+
+            else
+            {
+                shearMatrix = new double[,] {{1, factor}, {0, 1}};
+            }
+        }
+        else
+        {
+            return new double[,] { { -1 } };
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    newMatrix[i, j] += (matrix[i, k] * shearMatrix[k, j]);
+                }	
+            }
+        }
+		return newMatrix;
+    }
+}
