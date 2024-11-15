dd25e6052b6662f5950c698d210dc0de081ac827
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-11-15 16:08:47 -0500

    adding 0-queue again

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Class for Queue operations.
+/// </summary>
+class Queue<T>
+{
+    /// <summary>
+    /// Checks and returns the type of a Queue.
+    /// </summary>
+    /// <returns> A Queue's type. </returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
