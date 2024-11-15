e71677bb8f1daa0e160c9c76a8abaaa229a6b69d
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-11-15 14:41:31 -0500

    adding Readme and task 0

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Queue<T>
+{
+    /// <summary>
+    /// Checks and returns the Queue's type.
+    /// </summary>
+    /// <returns>
+    /// The Queue's type.
+    /// </returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
