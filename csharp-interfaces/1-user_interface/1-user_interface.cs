0a07ae8aac53965f66a2259f353d177020b5fa6b
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-11-17 13:22:40 -0500

    testing code

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,77 +1,58 @@
 ﻿using System;
 
-/// <summary>
-/// Represents a base class with a name property and a custom string representation.
-/// </summary>
+///<summary>Base class</summary>
 abstract class Base
 {
-    public string name = "";
+  public string name = "";
 
-    /// <summary>
-    /// Returns a string representation of the object.
-    /// </summary>
-    /// <returns>A string indicating the name and type of the object.</returns>
-    public override string ToString()
-    {
-        return (this.name + " is a " + this.GetType().ToString());
-    }
+	public override string ToString()
+	{
+		return (this.name + " is a " + this.GetType().ToString());
+	}
 }
 
-/// <summary>
-/// Interface for objects that can be interacted with.
-/// </summary>
+/// <summary> Interactive interface </summary>
 interface IInteractive
 {
     void Interact();
 }
 
-/// <summary>
-/// Interface for objects that can be broken.
-/// </summary>
+/// <summary> Breakable Interface </summary>
 interface IBreakable
 {
     int durability { get; set; }
     void Break();
 }
 
-/// <summary>
-/// Interface for objects that can be collected.
-/// </summary>
+/// <summary> Collectable Interface </summary>
 interface ICollectable
 {
     bool isCollected { get; set; }
     void Collect();
+
 }
 
-/// <summary>
-/// A class that represents a test object which can be interacted with, broken, and collected.
-/// </summary>
+/// <summary> Class that inherits from Base class and all interfaces </summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
-    public int durability { get; set; }
-    public bool isCollected { get; set; }
+	public int durability { get; set; }
+	public bool isCollected { get; set; }
 
-    /// <summary>
-    /// Takes place when the object interacts with something.
-    /// </summary>
-    public void Interact()
-    {
-        
-    }
+    /// <summary> Takes place when the object interacts with something </summary>
+	public void Interact()
+	{
 
-    /// <summary>
-    /// Takes place when the object breaks.
-    /// </summary>
-    public void Break()
-    {
-        
-    }
+	}
 
-    /// <summary>
-    /// Takes place when the object collects something.
-    /// </summary>
-    public void Collect()
-    {
-        
-    }
+    /// <summary> Takes place when the object brakes. </summary>
+	public void Break()
+	{
+
+	}
+
+    /// <summary> Takes place when the object collects something.</summary>
+	public void Collect()
+	{
+
+	}
 }
