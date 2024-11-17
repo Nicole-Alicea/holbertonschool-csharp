b633cb8c81a1f2dc7c78fa4a3f1c3526a158033e
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-11-17 15:24:52 -0500

    adding 4-check_yourself

---
+++ csharp-delegates_events/4-check_yourself/4-check_yourself.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Dynamic;
+
+/// <summary> A public class for the player. </summary>
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+    private string status;
+
+    /// <summary> Constructor </summary>
+    /// <param name="name"> The player's name. </param>
+    /// <param name="maxHp"> The player's maximum health points. </param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0f)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+        this.status = $"{this.name} is ready to go!";
+        HPCheck += CheckStatus;
+    }
+
+    private void CheckStatus(object sender, CurrentHPArgs e)
+    {
+        if (e.currentHp == this.maxHp)
+        {
+            this.status = $"{this.name} is in perfect health!";
+        }
+
+        else if (e.currentHp >= (this.maxHp * 0.5f) && e.currentHp < this.maxHp)
+        {
+            this.status = $"{this.name} is doing well!";
+        }
+
+        else if (e.currentHp >= (this.maxHp * 0.25f) && e.currentHp < (this.maxHp * 0.5f))
+        {
+            this.status = $"{this.name} isn't doing too great...";
+        }
+
+        else if (e.currentHp > 0f && e.currentHp < (this.maxHp * 0.5f))
+        {
+            this.status = $"{this.name} needs help!";
+        }
+
+        else
+        {
+            this.status = $"{this.name} is knocked out!";
+        }
+
+        Console.WriteLine(this.status);
+    }
+
+    /// <summary> Prints the player's health. </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    delegate void CalculateHealth(float damage);
+
+    /// <summary>
+    /// Handles the damage the player takes.
+    /// </summary>
+    /// <param name="damage"> The amount of damage the player takes. </param>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0f)
+        {
+            Console.WriteLine($"{name} takes 0 damage!");
+            damage = 0f;
+        }
+
+        else
+        {
+            Console.WriteLine($"{name} takes {damage} damage!");
+        }
+
+        this.hp -= damage;
+        ValidateHP(this.hp);
+    }
+
+    /// <summary>
+    /// Handles the player's healing.
+    /// </summary>
+    /// <param name="heal"> The amount the player will heal. </param>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0f)
+        {
+            Console.WriteLine($"{name} heals 0 HP!");
+            heal = 0f;
+        }
+
+        else
+        {
+            Console.WriteLine($"{name} heals {heal} HP!");
+        }
+
+        this.hp += heal;
+        ValidateHP(this.hp);
+    }
+
+    /// <summary>
+    /// Changes the player's HP to the appropriate one depending on the damage or healing taken.
+    /// </summary>
+    /// <param name="newHp"> The player's new HP value. </param>
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0f)
+        {
+            this.hp = 0f;
+        }
+
+        else if (newHp > this.maxHp)
+        {
+            this.hp = this.maxHp;
+        }
+
+        else
+        {
+            this.hp = newHp;
+        }
+
+        HPCheck(this, new CurrentHPArgs(this.hp));
+    }
+
+    /// <summary>
+    /// Applies a modifier to an attack.
+    /// </summary>
+    /// <param name="baseValue"> The normal attack power of the player. </param>
+    /// <param name="modifier"> Modifies the attack power of the player. </param>
+    /// <returns> The attack power. </returns>
+    public float ApplyModifier(float baseValue, Modifier modifier)
+    {
+        if (modifier == Modifier.Weak)
+        {
+            return baseValue * 0.5f;
+        }
+
+        else if (modifier == Modifier.Base)
+        {
+            return baseValue;
+        }
+
+        else
+        {
+            return baseValue * 1.5f;
+        }
+    }
+
+    public event EventHandler<CurrentHPArgs> HPCheck;
+}
+
+enum Modifier
+{
+    Weak,
+    Base,
+    Strong
+}
+
+/// <summary>
+/// Determines the strength of the attack modifier.
+/// </summary>
+/// <param name="baseValue"> Normal attack. </param>
+/// <param name="modifier"> Modifier that will be applied to the base value. </param>
+delegate float CalculateModifier(float baseValue, Modifier modifier);
+
+class CurrentHPArgs : EventArgs
+{
+    public readonly float currentHp;
+
+    public CurrentHPArgs(float newHp)
+    {
+        this.currentHp = newHp;
+    }
+}
