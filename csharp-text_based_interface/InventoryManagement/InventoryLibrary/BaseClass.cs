65583dae992034758e7757e051440983006f4d63
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-11-29 13:53:16 -0500

    adding documentation and adding method in base class

--- csharp-text_based_interface/InventoryManagement/InventoryLibrary/BaseClass.cs
+++ csharp-text_based_interface/InventoryManagement/InventoryLibrary/BaseClass.cs
@@ -1,9 +1,22 @@
 ﻿using System;
 
+/// <summary>
+/// Base class that all other classes will inherit from.
+/// </summary>
 public class BaseClass
 {
     // Properties
     public string id { get; set; }
     public DateTime date_created { get; set; }
     public DateTime date_updated { get; set; }
+
+    /// <summary>
+    /// Sets the values for the properties specified above.
+    /// </summary>
+    public BaseClass()
+    {
+        this.id = id;
+        this.date_created = DateTime.Now;
+        this.date_updated = DateTime.Now;
+    }
 }
